d63c542e7a9263968874ee2e48b1c3ab39d62531
Miftahul93 <miftahululum693@gmail.com>
2023-12-11 22:38:09 +0700

    Menulis pengujian lokal

--- app/src/main/java/com/athaya/tiptime/MainActivity.kt
+++ app/src/main/java/com/athaya/tiptime/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -126,7 +127,8 @@ fun TipTimeLayout() {
     }
 }
 
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp: Boolean
